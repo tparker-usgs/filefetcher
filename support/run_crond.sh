f79650b4c862a28705ea89652eb1e25fcd2b9802
Tom Parker <tparker@usgs.gov>
2018-07-18 14:08:26 -0800

    cleanup

--- support/run_crond.sh
+++ support/run_crond.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-LOG_NAME=`basename -s .yaml $CONFIG_FILE`
-
 # prepend application environment variables to crontab
 env | grep -v PATH | cat - /app/filefetcher/cron-filefetcher > /etc/cron.d/cron-filefetcher
 
