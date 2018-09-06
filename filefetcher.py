767e37a89390aefb8146acff5919a418b6013410
Tom Parker <tparker@usgs.gov>
2018-09-06 11:25:42 -0800

    tweak logging

--- filefetcher.py
+++ filefetcher.py
@@ -190,6 +190,8 @@ def poll_logger(datalogger, day):
         finished = fetch_file(c, out_path)
 
     time_exceeded = datetime.now() - START_TIME > global_config['maxRunTime']
+    if time_exceeded:
+        logger.info("maxRunTime exceeded, lets cleanup and exit.")
     return time_exceeded or (finished and backfill_finished(datalogger, day))
 
 
